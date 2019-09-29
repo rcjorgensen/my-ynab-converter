034dd65fe846627393132ec57b8e02a961303865
Rolf <rlfchr@gmail.com>
2019-09-29 12:41:28 +0200

    Add details page

---
+++ Application.RazorPages/Pages/Payees/Detail.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess;
+using Domain.Payee;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Application.RazorPages.Pages.Payees
+{
+    public class DetailModel : PageModel
+    {
+        private readonly IPayeeRepository payeeRepository;
+
+        public DetailModel(IPayeeRepository payeeRepository)
+        {
+            this.payeeRepository = payeeRepository;
+        }
+
+        public Payee Payee { get; set; }
+
+        public void OnGet(int payeeId)
+        {
+            Payee = payeeRepository.GetAll().Find(x => x.Id == payeeId);
+        }
+    }
+}
