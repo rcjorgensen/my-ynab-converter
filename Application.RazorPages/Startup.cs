a63e3e713e9ce5f0de61fbd905755ec2fd8b9b50
Rolf <rlfchr@gmail.com>
2019-10-12 20:21:15 +0200

    cleanup

--- Application.RazorPages/Startup.cs
+++ Application.RazorPages/Startup.cs
@@ -1,8 +1,6 @@
 using DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
